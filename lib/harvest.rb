4d54f0af5022aec6e4069ea561fdb918c0c0cf78
Paul Duvall <paul.duvall@stelligent.com>
2013-04-13 19:47:44 -0400

    added list of harvest time entries

--- lib/harvest.rb
+++ lib/harvest.rb
@@ -7,4 +7,12 @@ module Harvest
   def self.invoices_total
     GATEWAY.invoices.all.map{ |i| i.amount.to_f }.inject(0, :+)
   end
+  
+  def self.time_entries
+    project = 'Macys'
+    entries = GATEWAY.reports.time_by_project(project, Time.parse("04/08/2013"), Time.parse("04/14/2010"))
+    puts "Entries:"
+    entries.each {|e| p e}
+  end
+  
 end
